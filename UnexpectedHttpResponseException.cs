030bf73875cbd58a8b789bf6aeb1e9bc2c7bd9cc
Benjamin <benjaminjcharlton@gmail.com>
2021-07-12 12:50:08 +0400

    Added new exception class

---
+++ UnexpectedHttpResponseException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace Energetic.Http
+{
+    public class UnexpectedHttpResponseException : InvalidOperationException
+    {
+        public UnexpectedHttpResponseException(HttpStatusCode unexpectedCode, string reasonPhrase) :
+            base($"An unexpected response was received from an API. Status code: {unexpectedCode}.")
+        {
+            ResponseStatusCode = unexpectedCode;
+            ResponseReasonPhrase = reasonPhrase;
+        }
+
+
+        public HttpStatusCode ResponseStatusCode { get; private set; }
+
+        public string ResponseReasonPhrase { get; private set;  }
+    }
+}
